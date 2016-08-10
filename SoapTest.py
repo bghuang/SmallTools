fab120e74c856aacb63fcbc7daa43f0c0e857034
bghuang <bghuang@foxmail.com>
2016-08-10 23:37:10 +0800

    submit 1st version

---
+++ SoapTest.py
@@ -0,0 +1,26 @@
+import urllib2
+import xml.etree.ElementTree as ET
+
+soap_url = 'http://10.108.17.166:8210/SvcMgr'
+ns1 = "{http://alcatel-lucent.com/esm/ws/svcmgr/V2_0}"
+
+req = urllib2.Request(soap_url)
+
+loginReq = open('/shome/sms/soap_config/1.login.op.txt','r')
+contentText = loginReq.read()
+loginReq.close()
+
+
+req.add_header('Content-type', 'text/xml;charset=utf-8')
+resp = urllib2.urlopen(req, contentText)
+respXML = resp.read()
+
+ns ={'S': 'http://schemas.xmlsoap.org/soap/envelope/'}
+
+root = ET.fromstring(respXML)
+sessionid = root.findall('.//'+ns1+'sessionId')[0].text
+print sessionid
+
+tree = ET.parse("/shome/sms/soap_config/35.create_group_account.txt")
+root = tree.getroot()
+ET.dump(root)
